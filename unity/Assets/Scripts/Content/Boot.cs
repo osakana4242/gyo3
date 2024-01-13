779788141cfc3caae135a5124258adc7aaac5d83
hoge <example@example.com>
2024-01-13 21:29:40 +0900

    feat: unity2022

---
+++ unity/Assets/Scripts/Content/Boot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Osakana4242.UnityEngineExt;
+
+namespace Osakana4242.Content {
+	public class Boot : MonoBehaviour {
+		[SerializeField]
+		Camera camera_;
+		[SerializeField]
+		ParticleSystem.MinMaxGradient gradient;
+
+		IEnumerator Start() {
+			for ( int i = 0, iCount = 4; i < iCount; ++i ) {
+				Debug.Log( $"{i}" );
+				var t = ( ( float )i ) / ( iCount - 1 );
+				camera_.backgroundColor = gradient.Evaluate( t );
+				yield return 0;
+			}
+			UnityEngine.SceneManagement.SceneManager.LoadScene( "mainScene", UnityEngine.SceneManagement.LoadSceneMode.Single );
+		}
+	}
+}
